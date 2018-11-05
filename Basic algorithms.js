f785adaeb61f699ecfb443f0a0e5acd3c2d6670a
Vedrana <vedrana_ravlic16@yahoo.com>
2018-11-05 20:07:20 +0100

    Added the first 4 algorithms

---
+++ Basic algorithms.js
@@ -0,0 +1,51 @@
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
+
+/////////////////////
+
+function reverseString(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+reverseString("hello");
+
+////////////////////
+
+function factorialize(num) {
+  let res = num;
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  while (num > 1) {
+    num--;
+    res *= num;
+  }
+
+  return res;
+}
+
+factorialize(5);
+
+////////////////////
+
+function findLongestWordLength(str) {
+  let arr = str.split(" ").map(w => w.length);
+
+  arr.sort((a, b) => {
+    return b - a;
+  });
+  return arr[0];
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+/////////////////////
