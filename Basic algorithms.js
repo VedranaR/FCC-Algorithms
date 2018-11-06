3f40ccecfcbe73910cefd86b2f8f60efc84c6788
Vedrana <vedrana_ravlic16@yahoo.com>
2018-11-06 21:26:41 +0100

    Added 10  more

--- Basic algorithms.js
+++ Basic algorithms.js
@@ -72,3 +72,125 @@ largestOfFour([
 ]);
 
 ///////////////////
+//Basic Algorithm Scripting: Confirm the Ending
+////////////////
+
+function confirmEnding(str, target) {
+  let n = target.length;
+  return str.slice(-n) === target ? true : false;
+}
+
+confirmEnding("Bastian", "n");
+
+//Repeat a String Repeat a String
+//////////////////////
+
+function repeatStringNumTimes(str, num) {
+  let repeat = "";
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      repeat += str;
+    }
+  }
+  return repeat;
+}
+
+repeatStringNumTimes("abc", 3);
+
+//Truncate a String
+/////////////////
+
+function truncateString(str, num) {
+  let cut;
+  if (str.length > num) {
+    cut = str.slice(0, num);
+    return cut.concat("...");
+  } else if (str.length <= num) {
+    return (cut = str);
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+//Finders Keepers
+///////////////
+
+function findElement(arr, func) {
+  let test = arr.map(func);
+  let i = test.indexOf(true);
+  return arr[i];
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//Boo who
+/////////////
+
+function booWho(bool) {
+  return bool === true || bool === false ? true : false;
+}
+
+booWho(null);
+
+//Title Case a Sentence
+/////////////////////
+
+function titleCase(str) {
+  let arr = str.toLowerCase().split(" ");
+  let res = arr.map(function(item) {
+    return item.replace(item.charAt(0), item.charAt(0).toUpperCase());
+  });
+  return res.join(" ");
+}
+
+titleCase("I'm a little tea pot");
+
+//Slice and Splice
+////////////////
+
+function frankenSplice(arr1, arr2, n) {
+  let res = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    res.splice(n, 0, arr1[i]);
+    n++;
+  }
+
+  return res;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+//Falsy Bouncer
+////////////////////
+
+function bouncer(arr) {
+  arr = arr.filter(Boolean);
+  return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+//Where do I Belong
+/////////////////
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  return arr.indexOf(num);
+}
+
+getIndexToIns([40, 60], 50);
+
+//Mutations
+//////////////
+
+function mutation(arr) {
+  let item1 = arr[0].toLowerCase();
+  let item2 = arr[1].toLowerCase();
+
+  return item2.split("").every(letter => item1.indexOf(letter) >= 0);
+}
+
+mutation(["hello", "hey"]);
