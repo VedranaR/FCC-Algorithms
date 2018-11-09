297e61837bb1a4628fcbcd3d10c3ed050bff5c0f
Vedrana <vedrana_ravlic16@yahoo.com>
2018-11-09 23:41:57 +0100

    Added 2 more algorithms

--- Intermediate algorithms.js
+++ Intermediate algorithms.js
@@ -30,3 +30,16 @@ function diffArray(arr1, arr2) {
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+//Seek and Destroy
+//You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. //Remove all elements from the initial array that are of the same value as these arguments.
+
+//Note
+//You have to use the arguments object.
+
+function destroyer(arr) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  return arr.filter(item => args.indexOf(item) === -1);
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
