a44ed09e7392028981e13391158769a37f9c7930
Vedrana <vedrana_ravlic16@yahoo.com>
2018-11-13 23:38:15 +0100

    Making the files readable

--- JavaScript Algorithms and Data Structures Projects.js
+++ JavaScript Algorithms and Data Structures Projects.js
@@ -105,6 +105,7 @@ telephoneCheck("555-555-5555");
 // Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
 // Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
 
+//define the money dictionary
 let money = [
   { name: "ONE HUNDRED", val: 100 },
   { name: "TWENTY", val: 20 },
@@ -116,3 +117,66 @@ let money = [
   { name: "NICKEL", val: 0.05 },
   { name: "PENNY", val: 0.01 }
 ];
+
+//define the function that takes the input - price,
+//payment(cash) and the amount of cash in the drawer (cid)
+let checkCashRegister = (price, cash, cid) => {
+  //cid is a 2D array listing available currency
+  let result = { status: "", change: [] };
+  let change = cash - price;
+  let register = cid.reduce(
+    (acc, curr) => {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  if (register.total === change) {
+    result.status = "CLOSED";
+    result.change = cid;
+    return result;
+  }
+
+  if (register.total < change) {
+    result.status = "INSUFFICIENT_FUNDS";
+    return result;
+  }
+
+  let allChange = money.reduce((acc, curr) => {
+    let value = 0;
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+      change = Math.round(change * 100) / 100;
+    }
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc;
+  }, []);
+
+  if (allChange.length < 1 || change > 0) {
+    result.status = "INSUFFICIENT_FUNDS";
+    return result;
+  }
+
+  result.status = "OPEN";
+  result.change = allChange;
+  return result;
+};
+
+//test the function with the given input
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]);
